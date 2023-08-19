2753becf8793b7709e26e5857f326aabc700f4a4
Subhayan0022 <msubhayan529@gmail.com>
2023-08-19 14:27:06 +0530

    Initialized Project

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cerification</title>
+
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="flex h-full w-full mt-28 justify-center">
+        <img src="../src/assets/srmlogo.png" class="p-15 mr-8 h-4/6 w-4/12">
+    </div>
+    <div class ="flex flex-col  h-full w-full mt-24">
+        <div class="flex h-full w-full  justify-center">
+            <h1 class="first-text">SRM Institute of Science and Technology</h1>
+        </div>
+        <div class="flex h-full w-full justify-center">
+            <h1 class="second-text">Department of Science and Humanities</h1>
+        </div>
+    </div>
+    <div class="flex h-full w-full mt-28 justify-center">
+            <input type="number" class="flex bg-white border-2 border-black w-4/12 p-3 pl-4 rounded-2xl placeholder-gray-400" placeholder="Enter Registered Phone Number..." required/>
+            <button class="verify-btn ml-10 hover:scale-90 duration-500 rounded-xl px-8 ">VERIFY</button>
+    </div>
+
+</body>
+</html>
