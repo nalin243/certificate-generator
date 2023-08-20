38cac6673f095a0f23a253ee8d63e4ae6f409c3f
Subhayan0022 <msubhayan529@gmail.com>
2023-08-20 16:56:33 +0530

    Fixed number input UI

--- index.php
+++ index.php
@@ -15,8 +15,9 @@
     <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet">
 
     <script src="https://cdn.tailwindcss.com"></script>
+    
 </head>
-<body>
+<body OnLoad="document.phone.first.focus();">
     <div class="flex flex-col h-screen w-full border-4 border-black overflow-auto">
         <div class="flex flex-col h-full w-full border-4 border-black shrink-0">
             <div class="flex h-full w-full mt-28 justify-center">
@@ -30,9 +31,85 @@
                     <h1 class="second-text">Department of Science and Humanities</h1>
                 </div>
             </div>
-            <div class="flex h-4/6 w-full -mt-18 justify-center">
-                <input type="number" class="flex bg-white h-2/6 border-2 border-black w-4/12 pl-3 rounded-2xl placeholder-gray-400" placeholder="Enter Registered Phone Number..." required/>
+            <div id="inputs" class="inputs flex h-4/6 w-full -mt-18 justify-center">
+                <form id="phone" name="phone">
+                    <input class="input" name="first" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                    <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                </form>
                 <button class="verify-btn ml-5 hover:scale-90 h-2/6 px-3 ">VERIFY</button>
+            </div>
+            <script>
+                const inputs = document.getElementById("inputs");
+                const formElement = document.querySelector("#phone")
+
+                inputs.addEventListener("click",(event)=>{
+                    for(var i=0;i<event.target.length;i++){
+                        if(!event.target[event.target.length-1].value=="")
+                            event.target[event.target.length-1].focus()
+                        if(event.target[i].value==""){
+                            event.target[i].focus()
+                            break
+                        }
+                    }
+                })
+
+                inputs.addEventListener("keydown",(event)=>{
+                    const next = event.target.nextElementSibling
+                    const prev = event.target.previousElementSibling
+
+                    if(event.key=="ArrowRight"){
+                        if(next)
+                            next.focus()
+                    }
+                    if(event.key=="ArrowLeft"){
+                        if(prev)
+                            prev.focus()
+                    }
+                    
+                        
+                })
+                inputs.addEventListener("input", function (e) {
+                const target = e.target;
+                const val = target.value;
+                
+                if (isNaN(val)) {
+                    target.value = "";
+                    return;
+                }
+                
+                if (val != "") {
+                    const next = target.nextElementSibling;
+                    if (next) {
+                        next.focus();
+                    }
+                }
+                });
+                
+                inputs.addEventListener("keyup", function (e) {
+                const target = e.target;
+                const key = e.key.toLowerCase();
+                
+                if (key == "backspace" || key == "delete") {
+                    target.value = "";
+                    const prev = target.previousElementSibling;
+                    if (prev) {
+                        prev.focus();
+                        prev.value=""
+                    }
+                    return;
+                }
+                });
+            </script>
+            <div class="flex h-3/6 -mt-24 w-full justify-center">
+                <h1 class="text-black font-extrabold text-xl">Please Enter your Registered Phone Number.</h1>
             </div>
             <div class ="flex flex-col h-full w-full">
                 <div class="flex h-full w-full justify-center">
