63041bc2f9580d5eea388d739ec67cf2c4d66589
nalin243 <devpriyanalin@gmail.com>
2023-09-18 17:25:26 +0530

    Added modularity to code

---
+++ get_participants.php
@@ -0,0 +1,74 @@
+<?php
+    session_start();
+    
+    require 'db_config.php';
+
+    $client = new Google\Client;
+    $client->setAuthConfig("client_secret.json");
+    $client->setApplicationName("Certficate-generator");
+    $client->setScopes(['https://www.googleapis.com/auth/forms','https://www.googleapis.com/auth/drive']);
+    $service = new Google\Service\Forms($client);
+
+    $formIds = (($mysqli->query("select formId from users"))->fetch_all());
+
+    foreach($formIds as $formId){
+
+        try{
+            $form = $service->forms->get($formId[0]); 
+            $questionIds = [];
+
+
+            foreach($form['items'] as $item){
+                if(str_contains(strtolower($item['title']),"name"))
+                    $questionIds["name"] = $item['questionItem']['question']['questionId'];
+                if(str_contains(strtolower($item['title']),"date"))
+                    $questionIds["date"] = $item['questionItem']['question']['questionId'];
+                if(str_contains(strtolower($item['title']),"event"))
+                    $questionIds["eventname"] = $item['questionItem']['question']['questionId'];
+                if(str_contains(strtolower($item['title']),"class"))
+                    $questionIds["class"] = $item['questionItem']['question']['questionId'];
+                if(str_contains(strtolower($item['title']),"number"))
+                    $questionIds["number"] = $item['questionItem']['question']['questionId'];
+                if(str_contains(strtolower($item['title']),"mail"))
+                        $questionIds["mail"] = $item['questionItem']['question']['questionId'];
+                }
+
+            $responses = $service->forms_responses->listFormsResponses($formId[0]);
+            foreach($responses as $response){
+
+                $phoneId = $questionIds['number'];
+                $nameId = $questionIds['name'];
+                $eventNameId = $questionIds['eventname'];
+                $dateId = $questionIds['date'];
+                $mailId = $questionIds['mail'];
+                $classId = $questionIds['class'];
+
+                $phoneNo = (int)$response['answers']["$phoneId"]['textAnswers'][0]['value'];
+                $name = $response['answers']["$nameId"]['textAnswers'][0]['value'];
+                $eventName = $response['answers']["$eventNameId"]['textAnswers'][0]['value'];
+                $date = $response['answers']["$dateId"]['textAnswers'][0]['value'];
+                $mail = $response['answers']["$mailId"]['textAnswers'][0]['value'];
+                $class = $response['answers']["$classId"]['textAnswers'][0]['value'];
+
+                $result = $mysqli->query("select * from participants where pnumber=$phoneNo");
+
+                if(count($result->fetch_all())){
+                    //already exists so just go to the next response
+                    continue;
+                }
+                else {
+                    //does not exist so insert into db
+                    $mysqli->query("insert into participants(pnumber,name,date,eventname,formId,email,class) values($phoneNo,'$name','$date','$eventName','$formId[0]','$mail','$class')");
+                    }
+                }
+                $questionIds = [];
+
+            }catch(Exception $e){
+                if(gettype(strpos($e->getMessage(),"Requested entity was not found"))==="integer"){
+                    $_SESSION['errors']['form_error'] = "Whoops! ☹️ &nbsp; Incorrect Form ID! &nbsp; :(";
+                    header("Location: ./error_view.php");
+                }
+            }
+       }
+
+?>
